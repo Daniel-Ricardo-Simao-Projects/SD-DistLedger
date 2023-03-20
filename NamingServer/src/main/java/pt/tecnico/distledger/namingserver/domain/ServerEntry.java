760ecfbcf95b7ef5b41bfa0ba1b04ce79aee60a5
rtoscanelli <ricardotoscanelli@gmail.com>
2023-03-20 17:04:21 +0000

    Add server and service entry logic

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/ServerEntry.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/ServerEntry.java
@@ -1,3 +1,37 @@
+package pt.tecnico.distledger.namingserver.domain;
+
 public class ServerEntry {
 
+    private String target;
+
+    private String qualifier;
+
+    public ServerEntry(String target, String qualifier) {
+        this.target = target;
+        this.qualifier = qualifier;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public String getQualifier() {
+        return qualifier;
+    }
+
+    public void setQualifier(String qualifier) {
+        this.qualifier = qualifier;
+    }
+
+    @Override
+    public String toString() {
+        return "ServerEntry{" +
+                "target='" + target + '\'' +
+                ", qualifier='" + qualifier + '\'' +
+                '}';
+    }
 }
