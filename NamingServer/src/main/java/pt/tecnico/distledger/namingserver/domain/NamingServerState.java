6a1cd8701cbc6e3fe0c0967221eb425037166a76
rtoscanelli <ricardotoscanelli@gmail.com>
2023-03-20 17:08:34 +0000

    Add NamingServerState initial logic

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/NamingServerState.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/NamingServerState.java
@@ -1,5 +1,14 @@
 package pt.tecnico.distledger.namingserver.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NamingServerState {
 
+    private Map<String, ServiceEntry> servicesMap;
+
+    public NamingServerState() {
+        this.servicesMap = new HashMap<String, ServiceEntry>();
+    }
+    
 }
